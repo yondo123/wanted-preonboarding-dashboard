972478a5d0df173645827b4d496a0e4c640a1ef5
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-11-09 22:46:58 +0900

    feat: 라우터 및 상태관리 구성

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
-import App from './App';
+import colors from './styles/theme';
+import './assets/tailwind.css';
+import Layout from './pages/Layout';
+import Dashboard from './pages/Dashboard';
+import Advmgmt from './pages/Advmgmt';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <GlobalStyle />
-    <App />
-  </React.StrictMode>
+  <>
+    <ThemeProvider theme={colors}>
+      <GlobalStyle />
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/adv" element={<Advmgmt />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
+    </ThemeProvider>
+  </>
 );
